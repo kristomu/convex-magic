bd0411c239bcd912f9bb1ee9f7c17a5f5fae8902
Kristofer Munsterhjelm <kristofer@munsterhjelm.no>
2019-01-15 19:38:43 +0100

    Move LP construction to own function, remove using namespace std.

--- c++/fold_equalities.cc
+++ c++/fold_equalities.cc
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <eigen3/Eigen/Dense>
 #include <glpk.h>
-using namespace std;
 
 // Represents the polytope AFz <= bn, with x = Fz + x_0.
 
@@ -50,6 +49,76 @@ equality_reduction get_equality_reduction(const Eigen::MatrixXd & A,
 	return output;
 }
 
+// minimize cTx subject to Ax <= b. All variables are unconstrained.
+// GLPK syntax is ugly.
+
+std::pair<double, Eigen::VectorXd> linear_program(
+	const Eigen::MatrixXd & A, const Eigen::VectorXd & b, 
+	const Eigen::VectorXd & c) {
+
+	size_t A_size = A.rows() * A.cols();
+
+	// A in sparse representation
+	int row_idx[A_size], col_idx[A_size];
+	double value[A_size];
+
+	glp_prob *lp;
+	lp = glp_create_prob();
+	glp_set_prob_name(lp, "eigen_lp");
+	glp_set_obj_dir(lp, GLP_MIN);
+	glp_add_rows(lp, A.rows());
+	size_t i;
+
+	// set b
+	char rowname[] = "b_0";
+	for (i = 0; i < A.rows(); ++i) {
+		rowname[2] = i + '0';
+
+		glp_set_row_name(lp, i+1, rowname);
+  		glp_set_row_bnds(lp, i+1, GLP_UP, 0.0, b(i));
+	}
+
+	// set x and c
+	char colname[] = "x_0";
+	glp_add_cols(lp, A.cols());
+	for (i = 0; i < A.cols(); ++i) {
+		colname[2] = i + '0';
+		glp_set_col_name(lp, i+1, colname);
+		glp_set_col_bnds(lp, i+1, GLP_FR, 0.0, 0.0);
+		glp_set_obj_coef(lp, i+1, c(i));
+	}
+
+	// set A
+	int row, col;
+	i = 0;
+	for (row = 0; row < A.rows(); ++row) {
+		for (col = 0; col < A.cols(); ++col) {
+			if (A(row, col) == 0) {
+				continue;
+			}
+			row_idx[1+i] = row+1;
+			col_idx[1+i] = col+1;
+			value[1+i] = A(row, col);
+			++i;
+		}
+	}
+
+	// load and solve
+	glp_load_matrix(lp, i, row_idx, col_idx, value);
+	glp_simplex(lp, NULL); // solve the LP
+
+	// return value at optimal point, and that point.
+	Eigen::VectorXd x_opt(A.cols());
+
+	for (i = 0; i < A.cols(); ++i) {
+		x_opt(i) = glp_get_col_prim(lp, i+1);  		
+	}
+
+	double z = glp_mip_obj_val(lp);
+
+	return std::pair<double, Eigen::VectorXd>(z, x_opt);
+}
+
 /*
  Example:
 
@@ -88,87 +157,36 @@ int main()
 
 	equality_reduction reduction = get_equality_reduction(A, b, C, d);
 
-	cout << "A =\n" << A << "\n" << endl;
-	cout << "b =\n" << b << "\n" << endl;
-	cout << "C =\n" << C << "\n" << endl;
-	cout << "d =\n" << d << "\n" << endl;
+	std::cout << "A =\n" << A << "\n" << std::endl;
+	std::cout << "b =\n" << b << "\n" << std::endl;
+	std::cout << "C =\n" << C << "\n" << std::endl;
+	std::cout << "d =\n" << d << "\n" << std::endl;
 
-	cout << "---" << endl;
+	std::cout << "---" << std::endl;
 
-	cout << "AF=\n" << reduction.AF << "\n" << endl;
-	cout << "bn=\n" << reduction.bn << "\n" << endl;
-	cout << "F =\n" << reduction.F << "\n" << endl;
-	cout << "x_0 =\n" << reduction.x_0 << "\n" << endl;
+	std::cout << "AF=\n" << reduction.AF << "\n" << std::endl;
+	std::cout << "bn=\n" << reduction.bn << "\n" << std::endl;
+	std::cout << "F =\n" << reduction.F << "\n" << std::endl;
+	std::cout << "x_0 =\n" << reduction.x_0 << "\n" << std::endl;
 
-	cout << "---" << endl;
+	std::cout << "---" << std::endl;
 
 	// LP test, kinda kludgy
 	// min cTx s.t. Ax <= b, Cx = d.
 	// We know the classical solution is (0, 42, 8, 0), so let's try it
 	// with a reduced A (i.e. equality constraints folded in).
 
-	// GLPK syntax is ugly.
-
 	Eigen::Vector4d c;
 	c << 0, -1, 0, 0;
 
-	Eigen::VectorXd cF = c.transpose() * reduction.F;
+  	std::pair<double, Eigen::VectorXd> opt = linear_program(reduction.AF,
+  		reduction.bn, c.transpose() * reduction.F);
 
-	// A in sparse representation
-	int row_idx[1000], col_idx[1000];
-	double value[1000];
-
-	glp_prob *lp;
-	lp = glp_create_prob();
-	glp_set_prob_name(lp, "test");
-	glp_set_obj_dir(lp, GLP_MIN);
-	glp_add_rows(lp, 5);
-	int i;
-	// set b
-	char rowname[] = "b_0";
-	for (i = 0; i < 5; ++i) {
-		rowname[2] = i + '0';
-
-		glp_set_row_name(lp, i+1, rowname);
-  		glp_set_row_bnds(lp, i+1, GLP_UP, 0.0, reduction.bn(i));
-	}
-
-  	glp_add_cols(lp, 2);
-  	glp_set_col_name(lp, 1, "z1");
-  	glp_set_col_bnds(lp, 1, GLP_FR, 0.0, 0.0);
-  	glp_set_obj_coef(lp, 1, cF(0));
-  	glp_set_col_name(lp, 2, "z2");
-  	glp_set_col_bnds(lp, 2, GLP_FR, 0.0, 0.0);
-  	glp_set_obj_coef(lp, 2, cF(1));
-
-  	int row, col;
-  	i = 0;
-  	for (row = 0; row < 5; ++row) {
-  		for (col = 0; col < 2; ++col) {
-  			if (reduction.AF(row, col) == 0) {
-  				continue;
-  			}
-  			row_idx[1+i] = row+1;
-  			col_idx[1+i] = col+1;
-  			value[1+i] = reduction.AF(row, col);
-  			++i;
-  		}
-  	}
-  	glp_load_matrix(lp, i, row_idx, col_idx, value);
-  	glp_simplex(lp, NULL); // solve the LP
-
-  	Eigen::Vector2d z_opt;
-
-  	z_opt(0) = glp_get_col_prim(lp, 1);
-  	z_opt(1) = glp_get_col_prim(lp, 2);
-
-  	// Transform back.
-
-  	Eigen::VectorXd x_opt = reduction.F * z_opt + reduction.x_0;
+  	Eigen::VectorXd x_opt = reduction.F * opt.second + reduction.x_0;
   	Eigen::Vector4d should_be;
   	should_be << 0, 42, 8, 0;
 
-  	cout << x_opt << endl;
+  	std::cout << x_opt << std::endl;
 
   	if (x_opt == should_be) {
   		std::cout << "Test PASS" << std::endl;
